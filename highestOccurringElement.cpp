08425c8114af3163cf249fdc4ae7558d4817683a
Shivaay19 <zephyrus1920@gmail.com>
2025-06-12 18:51:12 +0530

    Second Commit

---
+++ highestOccurringElement.cpp
@@ -0,0 +1,17 @@
+#include "headers.h"
+int mostFrequentElement(vector<int>& nums) {
+        map<int,int> hashmap;
+        for(auto &i : nums) hashmap[i]++;
+        int max_frequency_element,max_frequency=-1;
+        for(auto it: hashmap){
+            if(it.second > max_frequency){
+                max_frequency_element = it.first;
+                max_frequency = it.second;
+            }
+        }
+        return max_frequency_element;
+    }
+int main(){
+    vector<int> nums = {2,2,1,1,1,6,4,3,3,3,5,4,4,4,4,6,3,3};
+    cout<<mostFrequentElement(nums)<<endl;
+}

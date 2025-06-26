c65b4b2c9aa085cf2dbf8dea02cd65b7e2654b2b
Shivaay19 <zephyrus1920@gmail.com>
2025-06-26 23:53:21 +0530

    Commit

---
+++ runLengthEncoding.cpp
@@ -0,0 +1,19 @@
+#include "headers.h"
+
+void encode(string &word){
+    int count = 1;
+   for(int i=0; i<word.length()-1; ++i){
+        if(word[i] == word[i+1]){
+            ++count;
+            continue;
+        }
+        cout<<word[i]<<count;
+        count = 1;
+    }
+    cout<<word[word.length()-1]<<count<<"\n";
+}
+
+int main(){
+    string word = "a";
+    encode(word);
+}
